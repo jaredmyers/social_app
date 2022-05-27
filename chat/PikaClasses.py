6e18f5e1e9d19926f38f87978abcba0059a14846
Jared Myers <jaredjmyers@gmail.com>
2022-05-27 12:20:03 -0400

    pika mq prep

--- chat/PikaClasses.py
+++ chat/PikaClasses.py
@@ -2,9 +2,13 @@
 
 import uuid
 import pika
+from db_accessor_methods import accessor_methods
 
 
 class RpcPublisher():
+    """
+    A class for sending messages to a specific rabbitMQ queue
+    """
 
     def __init__(self, user, pw, ip):
         self.credentials = pika.PlainCredentials(user, pw)
@@ -45,3 +49,42 @@ class RpcPublisher():
         while self.response is None:
             self.connection.process_data_events()
         return self.response
+
+
+class RunSubscriber():
+
+    def __init__(self, user, pw, ip):
+        self.credentails = pika.PlainCredentails(user, pw)
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(
+            ip,
+            5672,
+            'socialAppHost',
+            self.credentails)
+            )
+
+        self.channel = None
+
+    def rpc_subscribe(self, queue):
+
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=queue)
+
+        def send_to_accessor_methods(body, queue):
+            response = accessor_methods(body, queue)
+            return response
+
+        def on_request(ch, method, props, body):
+            print(" [.] message(%s)" % body)
+            response = send_to_accessor_methods(body, queue)
+
+            ch.basic_publish(exchange='',
+                             routing_key=props.reply_to,
+                             properties=pika.BasicProperties(
+                                correlation_id=props.correlation_id
+                             ),
+                             body=response)
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+
+        self.channel.basic_consume(queue=queue, on_message_callback=on_request)
+        print(" [x] awaiting rpc request..")
+        self.channel.start_consuming()
