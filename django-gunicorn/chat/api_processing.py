8b77080f9d7fbfc047c596ad2f951ffc88c62c16
Jared Myers <jaredjmyers@gmail.com>
2022-06-14 11:06:03 -0400

    deleted all unfinalized functions

--- django-gunicorn/chat/api_processing.py
+++ django-gunicorn/chat/api_processing.py
@@ -19,34 +19,6 @@ def get_recommended_friends(sessionID):
     return json.loads(matched_friends)
 
 
-"""
-def get_recommended_friends(sessionID):
-    '''gets recommened friends from api based on mutually liked music'''
-
-    # grab userID of current user based on session data
-    query = "select userID from sessions where sessionID=%s;"
-    val = (sessionID,)
-    cursor = conn.cursor()
-    cursor.execute(query, val)
-    query_result = cursor.fetchall()
-    userID = query_result[0][0]
-
-    # grab all other users from database for potential match
-    # this is done simply because this api is simulated
-    # and needs valid usernames from db to simulate friend matches
-
-    query = "select uname from users where userID not in (%s);"
-    val = (userID,)
-    cursor.execute(query, val)
-    query_result = cursor.fetchall()
-
-    matched_friends = []
-    for tup in query_result:
-        matched_friends.append(tup[0])
-
-    return matched_friends
-"""
-
 def get_recommended_details(sessionID, username):
     '''simulated api response since the api was cut from this sample'''
     '''this gets the details on which info the users were matched'''
@@ -68,18 +40,6 @@ def get_recommended_details(sessionID, username):
 
     return obj
 
-"""
-def get_recommended_details(sessionID, username):
-    '''simulated api response since the api was cut from this sample'''
-    '''this gets the details on which info the users were matched'''
-
-    with open("chat/static/details_simulated_api.json", "r") as file:
-        details = json.load(file)
-
-    obj = DisplayDetailsPage(details["tracks"], details["artists"], details["genres"], details["albums"], details["preview"])
-
-    return obj
-"""
 
 class DisplayDetailsPage():
 
